3c7780eb98a394c7112bdb66db6de8c537bd83d9
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-03-10 09:46:01 +0100

    feat: auth almost working

--- src/index.js
+++ src/index.js
@@ -1,124 +1,151 @@
-const {
-  BaseKonnector,
-  requestFactory,
-  scrape,
-  log,
-  utils
-} = require('cozy-konnector-libs')
-const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: false,
-  // This allows request-promise to keep cookies between requests
-  jar: true
+const { scrape, log, utils, solveCaptcha, CookieKonnector, BaseKonnector } = require('cozy-konnector-libs')
+const puppeteer = require('puppeteer')
+
+const VENDOR = 'Plutus'
+const baseUrl = 'https://dex.plutus.it'
+const dashboardUrl = `${baseUrl}/dashboard`
+const loginUrl = `${baseUrl}/auth/login`
+
+class PlutusConnector extends BaseKonnector {
+  async init() {
+    this.browser = await puppeteer.launch({ headless: false });
+    this.page = await this.browser.newPage();
+    await this.page.goto(baseUrl);
+
+    this.jwt = this.getAccountData().jwt
+    if (this.jwt) {
+      await this.page.evaluate((jwt) => localStorage.setItem("id_token", jwt), jwt)
+    }
+  }
+
+  static async make(options) {
+    let connector = new PlutusConnector(options)
+    await connector.init()
+    return connector
+  }
+
+  async testSession() {
+    try {
+      await this.page.goto(dashboardUrl)
+      await this.page.waitForSelector('a[href="/dashboard"]', { timeout: 3000 })
+      return true
+    } catch (err) {
+      return false
+    }
+  }
+
+  async authenticate(username, password, maybe_otp) {
+    await this.page.goto(loginUrl);
+    await this.page.waitForSelector('form')
+
+    const form = await this.page.$('form')
+
+    await form.$('input[name="email"]').then(el => el.type(username))
+    await form.$('input[name="password"]').then(el => el.type(password))
+    await form.$('button[type="submit"]').then(el => el.click())
+
+    await this.page.waitForSelector('input[name="code"]', { timeout: 2000 })
+
+    const two_fa_form = await this.page.$('form')
+    if (two_fa_form) {
+      await this.deactivateAutoSuccessfulLogin()
+
+      let code = maybe_otp
+      if (!code) {
+        code = await this.waitForTwoFaCode()
+      }
+      await two_fa_form.$('input[name="code"]').then(el => el.type(code))
+      await two_fa_form.$('button[type="submit"]').then(el => el.click())
+
+      await this.page.waitForNetworkIdle(3000)
+    }
+
+    this.jwt = await this.page.evaluate(() => localStorage.getItem("id_token"));
+    this.saveAccountData({ "jwt": this.jwt })
+
+    await this.notifySuccessfulLogin()
+  }
+
+  async fetch(fields) {
+    log('info', 'Fetching ...')
+    if (!(await this.testSession())) {
+      log('info', 'Authenticating ...')
+      await this.authenticate(fields.login, fields.password, fields.otp)
+    }
+    log('info', 'Successfully logged in')
+
+    await this.browser.close();
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${this.jwt} `);
+    myHeaders.append("Content-Type", "application/json");
+
+    const query = {
+      "operationName": "transactions_view",
+      "variables": {
+        "offset": 0,
+        "limit": null,
+        "from": null,
+        "to": null
+      },
+      "query": `
+      query transactions_view($offset: Int, $limit: Int, $from: timestamptz, $to: timestamptz) {
+  transactions_view_aggregate(
+    where: { _and: [{ date: { _gte: $from } }, { date: { _lte: $to } }] }
+  ) {
+    aggregate {
+      totalCount: count
+      __typename
+    }
+    __typename
+  }
+  transactions_view(
+    order_by: { date: desc }
+    limit: $limit
+    offset: $offset
+    where: { _and: [{ date: { _gte: $from } }, { date: { _lte: $to } }] }
+  ) {
+    id
+    model
+    user_id
+    currency
+    amount
+    date
+    type
+    is_debit
+    description
+    __typename
+  }
+} `
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(query),
+      redirect: 'follow'
+    };
+
+    try {
+      const response = await fetch("https://hasura.plutus.it/v1alpha1/graphql", requestOptions);
+      const result = await response.json();
+      console.log(JSON.stringify(result, null, 4));
+    }
+    catch (error) {
+      console.log('error', error);
+    }
+
+    log('info', 'Fetching the list of documents')
+
+    log('info', 'Saving data to Cozy')
+  }
+
+
+}
+
+const connector = await PlutusConnector.make({
+  cheerio: false,
+  json: false
 })
 
-const VENDOR = 'template'
-const baseUrl = 'http://books.toscrape.com'
-
-module.exports = new BaseKonnector(start)
-
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
-async function start(fields, cozyParameters) {
-  log('info', 'Authenticating ...')
-  if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
-  await authenticate.bind(this)(fields.login, fields.password)
-  log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
-
-  // Here we use the saveBills function even if what we fetch are not bills,
-  // but this is the most common case in connectors
-  log('info', 'Saving data to Cozy')
-  await this.saveBills(documents, fields, {
-    // This is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
-}
-
-// This shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return this.signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // The validate function will check if the login request was a success. Every website has a
-    // different way to respond: HTTP status code, error message in HTML ($), HTTP redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be useful for other connectors'
-      )
-      // The login in toscrape.com always works except when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
-  })
-}
-
-// The goal of this function is to parse a HTML page wrapped by a cheerio instance
-// and return an array of JS objects which will be saved to the cozy by saveBills
-// (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // You can find documentation about the scrape function here:
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // The saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: 'EUR',
-    filename: `${utils.formatDate(new Date())}_${VENDOR}_${doc.amount.toFixed(
-      2
-    )}EUR${doc.vendorRef ? '_' + doc.vendorRef : ''}.jpg`,
-    vendor: VENDOR
-  }))
-}
-
-// Convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
-}
+connector.run()
